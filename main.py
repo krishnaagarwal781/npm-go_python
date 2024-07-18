746a9db5f60baefcd07fa05095047b84c2fb9f68
Krishna <krishnacool781@gmail.com>
2024-07-18 18:19:54 +0530

    added get notice api

--- main.py
+++ main.py
@@ -404,3 +404,103 @@ async def get_collection_points(
                         purpose["purpose_date"] = purpose["purpose_date"].isoformat()
 
     return JSONResponse(content={"con_collection_points": collection_points})
+
+
+@app.get("/get-notice-info/{cp_id}")
+async def get_notice_info(cp_id: str, app_id: str, org_id: str, org_key: str, org_secret: str):
+    # Verify org_key and org_secret
+    organisation = developer_details_collection.find_one(
+        {"organisation_id": org_id, "org_key": org_key, "org_secret": org_secret}
+    )
+    if not organisation:
+        raise HTTPException(status_code=401, detail="Invalid org_key or org_secret")
+
+    # Retrieve collection point for the given cp_id, org_id, and app_id
+    collection_point = collection_point_collection.find_one({
+        "_id": ObjectId(cp_id),
+        "org_id": org_id,
+        "application_id": app_id
+    })
+
+    if not collection_point:
+        raise HTTPException(status_code=404, detail=f"Collection point with ID {cp_id} not found")
+
+    # Construct notice_info response for the specific collection point
+    notice_info = {
+        "urls": {
+            "logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI2K4cwj_yTWk-rSebFdFF-tX1yMKE8o_Uwnk5H9GYkIoqSKHAvt-pYaB1dEQHK1paNNk&usqp=CAU",
+            "speakIcon": "https://www.svgrepo.com/show/165176/speaking.svg",
+            "pauseIcon": "https://www.svgrepo.com/show/149256/pause-button.svg",
+            "arrowIcon": "https://cdn.icon-icons.com/icons2/2248/PNG/512/arrow_top_right_icon_135926.png",
+            "mp3Link": "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+            "dpar_link": "https://www.instagram.com",
+            "manage_consent_link": "https://www.facebook.com"
+        },
+        "english": {
+            "collection_point": {},
+            "meta_data": {
+                "header": "Left Notice",
+                "mp3Link": "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+                "title": "Cookies preferences center",
+                "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
+                "manage_consent_title": "Manage Consent Preferences"
+            },
+            "button": {
+                "primary": "Accept",
+                "secondary": "Cancel"
+            }
+        },
+        "french": {
+            "collection_point": {},
+            "meta_data": {},
+            "button": {}
+        },
+        "spanish": {
+            "collection_point": {},
+            "meta_data": {},
+            "button": {}
+        },
+        "german": {
+            "collection_point": {},
+            "meta_data": {},
+            "button": {}
+        }
+    }
+
+    # Populate collection point data in notice_info
+    data_elements = []
+    for de in collection_point.get("data_elements", []):
+        purposes = []
+        for purpose in de.get("purposes", []):
+            purposes.append({
+                "purpose_description": purpose.get("purpose_description", ""),
+                "purpose_language": purpose.get("purpose_language", "")
+            })
+        data_elements.append({
+            "data_element": de.get("data_element", ""),
+            "data_element_title": de.get("data_element_title", ""),
+            "data_element_description": de.get("data_element_description", ""),
+            "data_owner": de.get("data_owner", ""),
+            "legal_basis": de.get("legal_basis", ""),
+            "retention_period": de.get("retention_period", ""),
+            "cross_border": de.get("cross_border", False),
+            "sensitive": de.get("sensitive", False),
+            "encrypted": de.get("encrypted", False),
+            "expiry": de.get("expiry", ""),
+            "purposes": purposes
+        })
+
+    collection_point_info = {
+        "cp_id": str(collection_point["_id"]),
+        "cp_name": collection_point.get("cp_name", ""),
+        "cp_status": collection_point.get("cp_status", ""),
+        "cp_url": collection_point.get("cp_url", ""),
+        "data_elements": data_elements
+    }
+
+    notice_info["english"]["collection_point"] = collection_point_info
+    notice_info["french"]["collection_point"] = collection_point_info
+    notice_info["spanish"]["collection_point"] = collection_point_info
+    notice_info["german"]["collection_point"] = collection_point_info
+
+    return {"notice_info": notice_info}
